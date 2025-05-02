476ff628cdd62a2696caaa38bcfdcfe06f4c9bf5
WANDERSON FERREIRA DA SILVA <209373364+TicaoBaby@users.noreply.github.com>
2025-05-02 10:14:24 -0400

    Delete docs directory

--- docs/application/native/api/common/9.0/group__CAPI__MESSAGING__FRAMEWORK.js
+++
@@ -1,6 +0,0 @@
-var group__CAPI__MESSAGING__FRAMEWORK =
-[
-    [ "Email (Deprecated)", "group__CAPI__MESSAGING__EMAIL__MODULE.html", null ],
-    [ "Messages (Deprecated)", "group__CAPI__MESSAGING__MESSAGES__MODULE.html", "group__CAPI__MESSAGING__MESSAGES__MODULE" ],
-    [ "Push", "group__CAPI__MESSAGING__PUSH__PUBLIC__MODULE.html", null ]
-];
--- docs/application/native/api/common/9.0/group__CAPI__NETWORK__HTTP__MODULE.js
+++
@@ -1,4 +0,0 @@
-var group__CAPI__NETWORK__HTTP__MODULE =
-[
-    [ "HTTP Session (Deprecated)", "group__CAPI__NETWORK__HTTP__SESSION__MODULE.html", "group__CAPI__NETWORK__HTTP__SESSION__MODULE" ]
-];
